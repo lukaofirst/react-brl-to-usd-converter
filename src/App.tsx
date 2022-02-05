ac0e7c45af5f27990470875fdeffb254a9cc91cf
lukaofirst <lucasmacedo123@gmail.com>
2022-02-04 22:42:32 -0300

    implementing components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CurrencyRow from './CurrencyRow';
+
+const dolarValue = 5.33;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState<number>(0);
+
+    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (+e.target.value >= 0) {
+            const calcResult = (+e.target.value / dolarValue).toFixed(2);
+
+            setValue(+calcResult);
+        }
+    };
+
+    return (
+        <div className='card'>
+            <h2 className='title'>Convert BRL to USD</h2>
+            <CurrencyRow
+                onChange={onChangeHandler}
+                value={value}
+                moneyType='BRL'
+            />
+            <p className='output'>{value > 0 && `USD ${value}`}</p>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/CurrencyRow.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+
+type ICurrencyRow = {
+    moneyType: 'BRL';
+    value: number;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const CurrencyRow: FC<ICurrencyRow> = (props) => {
+    const { moneyType, value, onChange } = props;
+
+    return (
+        <div>
+            <label htmlFor='number' className='label-number'>
+                {moneyType}
+            </label>
+            <input
+                type='number'
+                id='number'
+                className='w-60'
+                onChange={onChange}
+            />
+        </div>
+    );
+};
+
+export default CurrencyRow;
